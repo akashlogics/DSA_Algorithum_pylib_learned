12f58e826fff15fb2497029f3bfde0cd8a22eee0
Akash R <akashaiprompt650@gmail.com>
2024-08-31 10:10:14 +0530

    feat:

--- python_DSA/trees.py
+++ python_DSA/trees.py
@@ -29,86 +29,25 @@ class BST:
             print('Value already in tree')
 
     def printtree(self):
-        print("Inorder")#LPR
-        self.inorder(self.root)
-        print("Preorder")
-
-    def _print_helper(self, node):
-        if node:
-            self._print_helper(node.right)
-            print(node.data)
-            self._print_helper(node.left)
-
-            #print("Preorder")#PLR
-            #print(node.data)
-            #self._print_helper(node.left)
-            #self._print_helper(node.right)
-#
-            #print("Postorder")#LRP
-            #self._print_helper(node.left)
-            #self._print_helper(node.right)
-            #print(node.data)
-
-# Create an instance of BST and insert some values
-t = BST()
-t.insert(50)
-t.insert(30)
-t.insert(20)
-t.insert(40)
-t.insert(70)
-t.insert(60)
-t.insert(80)
-
-# Print the tree
-t.printtree()
-
-class TreeNode:
-    def __init__(self, data):
-        self.data = data
-        self.left = None
-        self.right = None
-
-class BST:
-    def __init__(self):
-        self.root = None
-
-    def insert(self, data):
-        if not self.root:
-            self.root = TreeNode(data)
-        else:
-            self._insert_helper(self.root, data)
-
-    def _insert_helper(self, current, data):
-        if data < current.data:
-            if current.left is None:
-                current.left = TreeNode(data)
-            else:
-                self._insert_helper(current.left, data)
-        elif data > current.data:
-            if current.right is None:
-                current.right = TreeNode(data)
-            else:
-                self._insert_helper(current.right, data)
-        else:
-            print('Value already in tree')
-
-    def printtree(self):
         print("Inorder")
         self.inorder(self.root)
+        print()
         print("Preorder")
         self.preorder(self.root)
+        print()
         print("Postorder")
         self.postorder(self.root)
+        print()
 
-    def inorder(self, node):
+    def inorder(self, node):#LPR
         if node:
             self.inorder(node.left)
-            print(node.data)
+            print(node.data,end=" ")
             self.inorder(node.right)
 
     def preorder(self, node):
         if node:
-            print(node.data)
+            print(node.data,end=" ")
             self.preorder(node.left)
             self.preorder(node.right)
 
@@ -116,7 +55,7 @@ class BST:
         if node:
             self.postorder(node.left)
             self.postorder(node.right)
-            print(node.data)
+            print(node.data,end=" ")
 
 # Create an instance of BST and insert some values
 t = BST()
@@ -130,3 +69,4 @@ t.insert(80)
 
 # Print the tree
 t.printtree()
+
