76c949882273ad9e0c89f8593750621c05fda854
Akash R <akashaiprompt650@gmail.com>
2024-09-03 10:05:40 +0530

    feat:add levelorder and zigzagorder

--- python_DSA/trees.py
+++ python_DSA/trees.py
@@ -38,6 +38,12 @@ class BST:
         print("Postorder")
         self.postorder(self.root)
         print()
+        print("levelorder")
+        self.levelorder(self.root)
+        print()
+        print("zigzagorder")
+        self.zigzagorder(self.root)
+        print()
 
     def inorder(self, node):#LPR
         if node:
@@ -56,6 +62,40 @@ class BST:
             self.postorder(node.left)
             self.postorder(node.right)
             print(node.data,end=" ")
+    def levelorder(self, node):
+        if node is None:
+            return
+        queue = []
+        queue.append(node)
+        while queue:
+            temp = queue.pop(0)
+            print(temp.data, end=" ")
+            if temp.left:
+                queue.append(temp.left)
+            if temp.right:
+                queue.append(temp.right)
+        return
+    def zigzagorder(self, node):
+        if node is None:
+            return
+        queue = []
+        queue.append(node)
+        while queue:
+            size = len(queue)
+            for i in range(size):
+                temp = queue.pop(0)
+                print(temp.data, end=" ")
+                if i & 1:
+                    if temp.right:
+                        queue.append(temp.right)
+                    if temp.left:
+                        queue.append(temp.left)
+                else:
+                    if temp.left:
+                        queue.append(temp.left)
+                    if temp.right:
+                        queue.append(temp.right)
+        return
 
 # Create an instance of BST and insert some values
 t = BST()
@@ -66,7 +106,7 @@ t.insert(40)
 t.insert(70)
 t.insert(60)
 t.insert(80)
-
 # Print the tree
 t.printtree()
+
 
